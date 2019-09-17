f4d4edeb8f86b7b04c397a8b078020585cf0be0c
innaanikina <avril-avril@mail.ru>
2019-09-18 03:17:24 +0500

    Added schedule (needs some improvement)

--- src/Bot.java
+++ src/Bot.java
@@ -11,22 +11,56 @@ public class Bot {
         dictionary.put("Авторы", Commands::owners);
         dictionary.put("echo", Commands::echo);
         dictionary.put("quit", Commands::quit);
-        cmdStart(System.out::println);
+        dictionary.put("study", Commands::study);
+        //cmdStart(System.out::println);
+        newCmdStart(System.out::println);
+    }
+
+    public void newCmdStart(Consumer<String> print)
+    {
+        System.out.println("Welcome! Use 'help' to get help");
+        Scanner in = new Scanner(System.in);
+        var line = "";
+        while (!line.equals("quit"))
+        {
+            line = in.nextLine();
+            System.out.println(line);
+            try
+            {
+                print.accept(dictionary.get(line.split(" ")[0]).apply(line));
+            }
+            catch (NullPointerException e)
+            {
+                print.accept(dictionary.get("help").apply(line));
+            }
+        }
+
+        System.out.println("I am out");
+        in.close();
+        System.out.println("in is closed");
     }
 
     public void cmdStart(Consumer<String> print){
         Scanner in = new Scanner(System.in);
+        var command = "";
         while (true){
-            var command = in.nextLine();
-            try {
+            command = in.nextLine();
+            try
+            {
                 print.accept(dictionary.get(command.split(" ")[0]).apply(command));
-            }catch (NullPointerException e) {
+            }
+            catch (NullPointerException e)
+            {
                 print.accept(dictionary.get("help").apply(command));
+            }
+            if (command.equals("study")) {
+                continue;
             }
             if (command.split(" ")[0].equals("quit"))
                 break;
         }
         in.close();
+        System.out.println("EXIT");
     }
 
     public void findCommand(){
--- src/Commands.java
+++ src/Commands.java
@@ -1,3 +1,6 @@
+import schedulesrc.Schedule;
+import java.util.Scanner;
+
 public class Commands {
     public static String help(String command){
         return "Я могу:\n" +
@@ -17,5 +20,48 @@ public class Commands {
 
     public static String quit(String command){
         return "Пока-пока :_(";
+    }
+
+    public static String study(String command)
+    {
+        System.out.println("Напиши 'пары' - получи расписание\n" +
+                "somecommand - выход в главное меню");
+        Scanner input = new Scanner(System.in);
+        var word = "";
+        while(true)
+        {
+            word = input.nextLine();
+            System.out.println(word);
+            if (word.equals("пары"))
+            {
+                System.out.println("Введи день недели (в формате: Пн, Вт, Ср, Чт, Пт)");
+                var day = input.nextLine();
+                getSchedule(day);
+                System.out.println("Возврат в раздел учёбки (study)");
+            }
+            else if (word.equals("quit"))
+            {
+                System.out.println("I QUIT");
+                break;
+            }
+            else if (word.equals("help"))
+            {
+                System.out.println("Напиши 'пары' - получи расписание\n" +
+                        "somecommand - выход в главное меню");
+            }
+            else
+            {
+                System.out.println("Напиши 'help' - тебе помогут");
+            }
+        }
+        input.close();
+        System.out.println("end of study");
+        return "...Возврат в главное меню...";
+    }
+
+    public static void getSchedule(String day)
+    {
+        var daySchedule = new Schedule(day);
+        daySchedule.printDaySchedule();
     }
 }
