64d4b120faf1f3ae7dd17876e48f38709f1efd75
PavelCherentsov <pavelcherentsov23@gmail.com>
2019-09-18 22:11:42 +0500

    add game

--- src/Commands.java
+++ src/Commands.java
@@ -8,6 +8,7 @@ public class Commands {
                 "help - список возможностей\n" +
                 "Авторы - те, кто создал меня\n" +
                 "echo - повторю за тобой\n" +
+                "study - информация для учебы\n" +
                 "game - игра 'Виселица'\n" +
                 "quit - попрощаться";
     }
@@ -22,6 +23,45 @@ public class Commands {
 
     public static String quit(String command){
         return "Пока-пока :_(";
+    }
+
+    public static String study(Scanner in)
+    {
+        System.out.println("Напиши 'пары' - получи расписание\n" +
+                "quit - выход в главное меню");
+        var word = "";
+        while(true)
+        {
+            word = in.nextLine();
+            System.out.println(word);
+            if (word.equals("пары"))
+            {
+                System.out.println("Введи день недели (в формате: Пн, Вт, Ср, Чт, Пт)");
+                var day = in.nextLine();
+                getSchedule(day);
+                System.out.println("Возврат в раздел учёбки (study)");
+            }
+            else if (word.equals("quit"))
+            {
+                break;
+            }
+            else if (word.equals("help"))
+            {
+                System.out.println("Напиши 'пары' - получи расписание\n" +
+                        "quit - выход в главное меню");
+            }
+            else
+            {
+                System.out.println("Напиши 'help' - тебе помогут");
+            }
+        }
+        return "...Выход в главное меню...";
+    }
+
+    public static void getSchedule(String day)
+    {
+        var daySchedule = new Schedule(day);
+        daySchedule.printDaySchedule();
     }
 
     public static String game(String command) {
