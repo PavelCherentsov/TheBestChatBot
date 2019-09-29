16066918c7c2deae745baaf881280938ce77534e
PavelCherentsov <pavelcherentsov23@gmail.com>
2019-09-30 03:21:13 +0500

    update Status

--- src/main/java/Status.java
+++ src/main/java/Status.java
@@ -1,18 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public enum Status {
     MENU,
     GAME;
-
-    private static Status active = Status.MENU;
-
-    public static Status getStatus() {
-        return active;
-    }
-
-    public static void setStatus(Status status) {
-        active = status;
-    }
-
 }
