37e78df4a0e6c5890ab5704ee0983379dfa5b319
PavelCherentsov <pavelcherentsov23@gmail.com>
2019-10-01 21:25:54 +0500

    add telegram api

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,10 +1,32 @@
 import java.util.HashMap;
-import java.util.Scanner;
 import java.util.function.Function;
 
 import Commands.*;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
-public class Bot {
+public class Bot extends TelegramLongPollingBot {
+
+    private static final String BOT_NAME = "@Povel";
+    private static final String BOT_TOKEN = "986971120:AAH8ZN8nrGjjxdMIXA0hEDF6Q0HXNuXP9gI";
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        Bot bot = new Bot();
+        try {
+            telegramBotsApi.registerBot(bot);
+
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
     private Status statusActive;
 
     private HashMap<String, Function<String, String>> dictionary = new HashMap<String, Function<String, String>>();
@@ -16,30 +38,33 @@ public class Bot {
         dictionary.put("quit", Quit::quit);
         dictionary.put("game", Hangman::game);
         dictionary.put("study", Study::study);
-        cmdStart();
     }
 
-    public void cmdStart() {
-        Scanner in = new Scanner(System.in, "Cp866");
-        String command;
-        String result = "";
-        while (true) {
-            String line = in.nextLine();
+    @Override
+    public void onUpdateReceived(Update update) {
+        update.getUpdateId();
+
+        SendMessage sendMessage = new SendMessage().setChatId(update.getMessage().getChatId());
+        try {
+            String command;
+            String result = "";
             try {
-                if (line.equals("/start")) {
+                if (update.getMessage().getText().equals("/start")) {
                     result = "Привет, я бот! Напиши '/help', и я расскажу, что умею :)";
                     statusActive = Status.MENU;
                 } else if (statusActive == Status.MENU) {
-                    command = line.split(" ")[0];
-                    result = dictionary.get(command).apply(line);
+                    command = update.getMessage().getText().split(" ")[0];
+                    result = dictionary.get(command).apply(update.getMessage().getText());
                     if (command.equals("game")) {
                         statusActive = Status.GAME;
                     }
-                    if (command.equals("quit"))
-                        break;
+                    if (command.equals("quit")) {
+                        result = "Пока";
+                        statusActive = null;
+                    }
                 } else if (statusActive == Status.GAME) {
-                    result = Hangman.game(line);
-                    if (line.equals("quit") || result.equals("Поздравляю! Ты выиграл! :)")
+                    result = Hangman.game(update.getMessage().getText());
+                    if (update.getMessage().getText().equals("quit") || result.equals("Поздравляю! Ты выиграл! :)")
                             || result.equals("Ты проиграл(")) {
                         statusActive = Status.MENU;
                     }
@@ -47,10 +72,23 @@ public class Bot {
                     result = "Для запуска напишите '/start'";
                 }
             } catch (NullPointerException e) {
-                result = dictionary.get("/help").apply(line);
+                result = dictionary.get("/help").apply(update.getMessage().getText());
             }
-            System.out.println(result);
+            sendMessage.setText(result);
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
-        in.close();
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
     }
 }
